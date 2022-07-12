024ccfa1deb404f3fbd27d1fc7fd086ce61840aa
VTramon <vitorr.ooliveira@gmail.com>
2022-07-12 20:19:59 -0300

    customized the header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,21 @@
+import styles from './style.module.scss'
+
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header className={styles.header}>
+      <nav className={styles.header__nav}>
         <li>
           <a href="#home">Home</a>
         </li>
+
         <li>
           <a href="#about">About</a>
         </li>
+
         <li>
           <a href="#projects">Projects</a>
         </li>
+
         <li>
           <a href="#contacts">Contacts</a>
         </li>
