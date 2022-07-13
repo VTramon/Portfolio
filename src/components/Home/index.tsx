28eb463133bb8b45de214f659e1adb72241cfee5
VTramon <vitorr.ooliveira@gmail.com>
2022-07-13 14:32:12 -0300

    customized the home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,14 +1,22 @@
+import styles from './style.module.scss'
+
 const Home = () => {
   return (
-    <section id="home">
-      <div>
+    <section id="home" className={styles.home}>
+      <div className={styles.home__banner}>
         <h1>
-          Front end|mobile <strong>developer</strong>
+          Front end | Mobile
+          <br />
+          <strong>Developer</strong>
         </h1>
       </div>
-      <div>
-        <h2>Vitor Ramon</h2>
-        <img src="/notebook.png" alt="notebook" width="700" />
+
+      <div className={styles.home__image_container}>
+        <div>
+          <h2>Vitor Ramon</h2>
+
+          <img src="/notebook.png" alt="notebook" width="700" />
+        </div>
       </div>
     </section>
   )
