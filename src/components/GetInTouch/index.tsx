20c833002e53e00be50226f7b631babd3958492a
VTramon <vitorr.ooliveira@gmail.com>
2022-07-26 22:38:44 -0300

    added a form to the GetInTouch component

--- src/components/GetInTouch/index.tsx
+++ src/components/GetInTouch/index.tsx
@@ -1,8 +1,36 @@
+import emailjs from '@emailjs/browser'
+import { FormEvent } from 'react'
 import { useTranslation } from 'react-i18next'
 import styles from './style.module.scss'
 
 const GetInTouch = () => {
   const { t } = useTranslation()
+
+  const sendEmail = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID!,
+        process.env.REACT_APP_TEMPLATE_ID!,
+        event.currentTarget,
+        process.env.REACT_APP_PUBLIC_KEY!
+      )
+      .then(
+        (result) => {
+          window.alert('Email enviado com sucesso.')
+          event.currentTarget.reset()
+        },
+        (error) => {
+          window.alert('Houve algo de errado, por favor tente novamente.')
+          console.log(error.text)
+        }
+      )
+      .catch((error) => {
+        window.alert('Houve algo de errado, por favor tente novamente.')
+        console.log(error)
+      })
+  }
 
   return (
     <section id="GetInTouch" className={styles.GetInTouch}>
@@ -11,14 +39,27 @@ const GetInTouch = () => {
 
         <p>{t('contact_text')}</p>
 
-        <div className={styles.GetInTouch__container_button}>
-          <div className={styles.GetInTouch__background_button} />
-          <button>
-            <a href="mailto:vitorr.ooliveira@gmail.com">
-              {t('contact_button')}
-            </a>
-          </button>
-        </div>
+        <form
+          onSubmit={sendEmail}
+          name="emailForm"
+          className={styles.GetInTouch__form}
+        >
+          <input type="text" placeholder="Name" name="name" required />
+
+          <input placeholder="Email" name="email" type="email" required />
+
+          <textarea
+            placeholder="Message"
+            name="message"
+            id="mailContent"
+            required
+          />
+
+          <div className={styles.GetInTouch__container_button}>
+            <div className={styles.GetInTouch__background_button} />
+            <button type="submit">{t('contact_button')}</button>
+          </div>
+        </form>
       </div>
     </section>
   )
